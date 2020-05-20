97acd2ece9ee5a853c209cc8f9b748273719cf88
Lauren Emick <laurenemick6@gmail.com>
2020-05-20 09:04:03 -0700

    stretch complete in stretch.js folder

--- index.js
+++ index.js
@@ -152,24 +152,22 @@ console.log(getAverageGoals(fifaData));
 
 
 
-
 /// STRETCH 🥅 //
 
-/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
+/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` 
+and `team initials` and returns the number of world cup wins that country has had. 
 
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
-function getCountryWins(/* code here */) {
+function getCountryWins() {
 
-    /* code here */
-
-};
 
 getCountryWins();
 
 
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
+/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns 
+the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
 function getGoals(/* code here */) {
 
@@ -180,7 +178,9 @@ function getGoals(/* code here */) {
 getGoals();
 
 
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
+/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and 
+calculates the team with the most goals scored against them per appearance 
+(average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
 
@@ -190,4 +190,5 @@ function badDefense(/* code here */) {
 
 badDefense();
 
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
+/* If you still have time, use the space below to work on any stretch goals of your chosing 
+as listed in the README file. */
